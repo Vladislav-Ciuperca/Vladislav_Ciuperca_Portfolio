96a31a5da3c484b3867e01957f46ec8312278bc3
Vladislav Ciuperca <ciuperca37@gmail.com>
2024-10-22 01:10:40 +0200

    genero funzione che crea 2 numeir diversi

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -8,34 +8,43 @@ use App\Models\project;
 
 class ProjectSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-
+    //  funzione ceh genera 2 numeri diversi //
+    public function randomNumbers()
+    {
+        $numbers = [];
+        for ($i = 0; $i < 2; $i++) {
+            $randomNum = random_int(1, 6);
+            if (!in_array($randomNum, $numbers)) {
+                $numbers[] = $randomNum;
+            } else {
+                $i--;
+            }
+        }
+        return $numbers;
+    }
 
     public function run(): void
     {
 
-        for ($i=0; $i < 15; $i++) { 
-            
+        for ($i = 0; $i < 15; $i++) {
+
             $new_project = new project();
-            $new_project -> name = 'progetto_'.$i+1;
-            $new_project -> img = 'https://picsum.photos/id/'.random_int(1,1083).'/600/300';
-            $new_project -> description = 'progetto molto bello blabalaba';
-            $new_project -> codeLink = 'https://github.com/Vladislav-Ciuperca/Vladislav_Ciuperca_Portfolio';
-            $new_project -> category_id = random_int(1,3);
-            
-            $new_project ->save();
+            $new_project->name = 'progetto_' . $i + 1;
+            $new_project->img = 'https://picsum.photos/id/' . random_int(1, 1083) . '/600/300';
+            $new_project->description = 'progetto molto bello blabalaba';
+            $new_project->codeLink = 'https://github.com/Vladislav-Ciuperca/Vladislav_Ciuperca_Portfolio';
+            $new_project->category_id = random_int(1, 3);
 
-            $nuemri = [
-                random_int(1,6),
-                random_int(1,6)
-            ];
+            $new_project->save();
 
-            
-            $new_project -> technologies()->attach( $nuemri );
+            // $nuemri = [
+            //     random_int(1, 6),
+            //     random_int(1, 6)
+            // ];
+
+
+            $new_project->technologies()->attach($this->randomNumbers());
             // $new_project -> technologies()->attach( random_int(1,6 ));
         }
-
     }
 }
